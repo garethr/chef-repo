f97f66445b4cb48c6ad0f68c83009a188fd1d4c9
Gareth Rushgrove <gareth@morethanseven.net>
2010-12-21 17:03:12 +0000

    Working version including sample zsh and vim configs

--- cookbooks/garethr/recipes/default.rb
+++ cookbooks/garethr/recipes/default.rb
@@ -1,5 +1,36 @@
+
 %w{zsh wget curl git-core ack vim}.each do |pkg|
   package pkg do
     action :install
   end
 end
+
+git "/home/vagrant/.oh-my-zsh" do
+  repository "https://github.com/robbyrussell/oh-my-zsh.git"
+  action :checkout
+  user "vagrant"
+  group "vagrant"
+end
+
+execute ".zshrc" do
+  command "cp /home/vagrant/.oh-my-zsh/templates/zshrc.zsh-template /home/vagrant/.zshrc"
+  creates "/home/vagrant/.zshrc"
+  user "vagrant"
+end
+
+file "/etc/profile.d/zsh.sh" do
+  content "export SHELL=/bin/zsh"
+  action :create
+end
+
+git "/home/vagrant/.vim" do
+  repository "https://github.com/jtimberman/dotvim.git"
+  action :checkout
+  user "vagrant"
+end
+
+link "/home/vagrant/.vimrc" do
+  to "/home/vagrant/.vim/vimrc"
+  owner "vagrant"
+end
+
